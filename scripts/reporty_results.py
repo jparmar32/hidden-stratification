3b4611a4f536e6046adf14bdacd2dd81a5cf38b2
jparmar32 <jsparmar@stanford.edu>
2021-06-01 20:09:59 -0700

    Finish Group DRO experiments

--- scripts/reporty_results.py
+++ scripts/reporty_results.py
@@ -5,16 +5,41 @@ import os
 
 train_set = 'cxr_p'
 test_set = 'cxr_p'
-results_dir = f'/mnt/gaze_robustness_results/gdro/cxr_p/erm'
+results_dir = f'/mnt/gaze_robustness_results/gdro/cxr_p/true_subclass_gdro'
 
 seeds = [x for x in range(10)] 
-lrs = [.1, .01, .001, .0001]
-wds = [1, .1, .01, .001, .0001, 0]
+wds = [10, 1, .1, .01, .001, .0001]
+
+
+
+best_subclass_acc = 0.0
+best_wd = 0
+for wd in wds:
+    subclass_acc_means = []
+    for cv in seeds: 
+        wd_string = str(wd)
+        wd_string = wd_string.strip("0")
+           
+        if wd == 10:
+            res_file = f"/mnt/gaze_robustness_results/gdro/cxr_p/wd_10/true_subclass_gdro/seed_{cv}/metrics.json"
+        else:
+            res_file = f"/mnt/gaze_robustness_results/gdro/cxr_p/wd_{wd_string}/true_subclass_gdro/seed_{cv}/metrics.json"
+        
+        with open(res_file) as data_file:
+            results = json.load(data_file)
+            subclass_acc_means.append(results['test']['subclass_rob_acc'])
+    mean = np.mean(subclass_acc_means)
+    print(f"wd: {wd} with mean subclass acc: {mean}")
+    if mean >= best_subclass_acc:
+        best_subclass_acc = mean
+        best_wd = wd
+print(f"best wd: {best_wd} with mean subclass acc: {best_subclass_acc}")
 
 avg_acc_means = []
 subclass_acc_means = []
 
 #do overall as well as subclass
+results_dir = f'/mnt/gaze_robustness_results/gdro/cxr_p/wd_{best_wd}/true_subclass_gdro'
 
 for cv in seeds:
     res_file = os.path.join(results_dir, f"seed_{cv}/metrics.json")
--- scripts/run_gas.sh
+++ scripts/run_gas.sh
@@ -1,5 +1,5 @@
-#mode=true_subclass_gdro
-mode=erm
+mode=true_subclass_gdro
+#mode=erm
 for wd in 10 1 .1 .01 .001 .0001
     do
     for seed in 0 1 2 3 4 5 6 7 8 9
